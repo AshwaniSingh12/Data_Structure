3ef23811ed988433403e5b46adb99f136070f637
Ashwani Singh <as3141538@gmail.com>
2023-06-30 20:21:03 +0530

    git ignore commit

---
+++ OOPs/first.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Hero {
+
+    //char name[100];
+    int health;
+    char level;
+
+};
+
+int main()
+{
+    Hero ramesh;
+    cout<<"health is : "<<ramesh.health <<endl;
+    cout<<"Level is : "<<ramesh.level <<endl;
+}
