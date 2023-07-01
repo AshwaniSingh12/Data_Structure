62bb4fe141183ae3c9ed4745584c58f275770cbb
Ashwani Singh <as3141538@gmail.com>
2023-07-01 20:52:45 +0530

    git ignore commit

--- OOPs/Hero_Class.cpp
+++ OOPs/Hero_Class.cpp
@@ -1,6 +1,24 @@
 class Hero {
-
+    private:
     int health;
+
+    public:
     char level;
+    int getHealth()
+    {
+        return health;
+    }
+    char getLevel(){
+        return level;
+    }
+
+    void setHealth(int h)
+    {
+        health = h;
+    }
+    void setLevel(char ch){
+        level = ch;
+    }
+
 
 };
--- OOPs/getter_setter.cpp
+++ OOPs/getter_setter.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
     Hero amit;
-    amit.health = 70;
-    amit.level = 20;
+    //amit.health = 70;
+    amit.level = 'A';
     
     cout<<" Size : " << sizeof(amit) << endl;
     
-    cout<<" Health : " << amit.health << endl;
+    cout<<" Health : " << amit.getHealth() << endl;
     cout<<" Level : " << amit.level << endl;
 }
